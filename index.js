1a20db4cc4a495a1564ad754661616d666359c8a
JLW93 <wornerjl@gmail.com>
2023-06-28 11:06:27 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+const pass = require('./pass.json');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors({
+    origin: 'http://127.0.0.1:5500'
+}));
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'jcmlawnservices.ky@gmail.com',
+        pass: pass.pass
+    }
+});
+
+app.post('/api/data/email', async (req, res) => {
+    try {
+        const requestData = req.body;
+        console.log(requestData);
+
+        if(!requestData.phone.includes('-')) {
+            var phoneNumber = `(${requestData.phone.slice(0,3)}) ${requestData.phone.slice(3,6)}-${requestData.phone.slice(6)}`
+        } else {
+            var phoneNumber = requestData.phone;
+        }
+
+        if(requestData.desired_service == 'quote') {
+            const mailOptions = {
+                from: 'jcmlawnservices.ky@gmail.com',
+                to: 'jcmlawnservices.ky@gmail.com',
+                subject: '[QUOTE] Customer Contact Request',
+                html: 
+                `
+                <h1>Name: ${requestData.first_name} ${requestData.last_name}</h1>
+                <h2>Phone Number: ${phoneNumber}</h2>
+                <h3>Email Address: ${requestData.email}</h2>
+                <h3>Requested date of service: ${requestData.date}</h3>
+                <h3>Service(s) requested: ${requestData.type_of_service}</h3>
+                <p><strong>Message:</strong> ${requestData.message}</p>
+                `
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    console.log('Email sent:', info.response);
+                }
+            });
+
+        } else if(requestData.desired_service == 'schedule_service') {
+            const mailOptions = {
+                from: 'jcmlawnservices.ky@gmail.com',
+                to: 'jcmlawnservices.ky@gmail.com',
+                subject: '[SERVICE] Customer Contact Request',
+                html: 
+                `
+                <h1>Name: ${requestData.first_name} ${requestData.last_name}</h1>
+                <h2>Phone Number: ${phoneNumber}</h2>
+                <h2>Email Address: ${requestData.email}</h2>
+                <h3>Requested date of service: ${requestData.date}</h3>
+                <h3>Service(s) requested: ${requestData.type_of_service}</h3>
+                <p><strong>Message:</strong> ${requestData.message}</p>
+                `
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    console.log('Email sent:', info.response);
+                }
+            });
+        }
+
+        res.json({success: true});
+
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
