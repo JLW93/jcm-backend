fa49160283276461d2a4815244eb69fd4984d377
JLW93 <wornerjl@gmail.com>
2023-06-30 11:13:26 -0400

    edited CORS origin with new domain

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5500'
+    origin: 'https://jcm-frontend.vercel.app/'
 }));
 
 const transporter = nodemailer.createTransport({
